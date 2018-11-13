6698093b6bccc2cd1ba32425afdeb67b0b1f42aa
Architekton <architekton350@gmail.com>
2018-11-13 14:59:50 +1100

    hasty script

--- sc_resolve.sh
+++ sc_resolve.sh
@@ -1,12 +1,4 @@
-#!/bin/sh
-
-if [ -n "$(which pacman 2> /dev/null)" ]; then
-	if [ -z "$(pacman -Q bind-tools 2> /dev/null)" ]; then
-		sudo pacman -S bind-tools
-		echo "Re-run this script."
-		exit 0
-	fi
-fi
+#!/usr/bin/env sh
 
 if [ -z "$1" ]; then
 	echo "Missing argument."
@@ -14,6 +6,6 @@ if [ -z "$1" ]; then
 fi
 
 #Resolve to an ip
-dig +short "$1" | 
+dig +short "$1" |
 grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' |
 head -1
